d9956c9db467b3240aa0cb3aea7cb13d1bec2cc2
Joel Dumas <joel.dumas@elca.ch>
2021-04-14 19:19:35 +0200

    "Nulls" for naming consistency with other option

--- Tests/Base/TestBase.cs
+++ Tests/Base/TestBase.cs
@@ -1614,12 +1614,12 @@ namespace Tests
 	{
 		public WithoutContainsNullCheck()
 		{
-			Configuration.Linq.CheckNullInContains = false;
+			Configuration.Linq.CheckNullsInContains = false;
 		}
 
 		public void Dispose()
 		{
-			Configuration.Linq.CheckNullInContains = true;
+			Configuration.Linq.CheckNullsInContains = true;
 			Query.ClearCaches();
 		}
 	}
